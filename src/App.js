82a91920f8bcc910ca65cea13d35b89a7bca8bf2
Neeraj Anand <56186425+neeraj-2@users.noreply.github.com>
2020-11-15 11:13:27 +0530

    Initial-commit-navbar-added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MyNavbar from './components/my-navbar/mynavbar.component';
+
+
+const App=()=> {
+  return (
+    <div>
+      <MyNavbar/>
+      
+    </div>
+  );
+}
+
+export default App;
