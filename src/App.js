746929515277d2f27c914f5600ca962ccad167ea
Neeraj Anand <56186425+neeraj-2@users.noreply.github.com>
2020-11-15 14:43:45 +0530

    about_page added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import './App.css';
 import MyNavbar from './components/my-navbar/mynavbar.component';
 import MyCarousal from './components/my-carousal/my-carousal.component';
 import TitleMessage from './components/title-message/title-message.component'
+import About from './pages/about/about.component';
+import { Parallax } from "react-parallax";
+import Container from "react-bootstrap/Container";
+import Fade from "react-reveal/Fade";
 
 const App=()=> {
   return (
-    <div>
+    <div className="App" style={{ position: "relative" }}>
       <MyNavbar/>
       <MyCarousal />
-      <TitleMessage/>
+      <TitleMessage />
+      <div>
+        <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/parallex/background.webp")}
+          bgImageAlt=""
+          strength={-200}
+        >
+          <div>
+            <Container className="container-box rounded">
+              <Fade duration={500}>
+                <About />
+              </Fade>
+            </Container>
+          </div>
+        </Parallax>
+      </div>
     </div>
   );
 }
