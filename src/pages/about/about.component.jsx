d15d73f485990a4da22300c7fd4c5c33bf696a5f
Neeraj Anand <56186425+neeraj-2@users.noreply.github.com>
2020-12-13 12:20:40 +0530

    Resume _added

--- src/pages/about/about.component.jsx
+++ src/pages/about/about.component.jsx
@@ -37,7 +37,7 @@ const About = () => {
                     </a>
                   </div>
                   <div>
-                    <a href="#" target="_blank" rel="noopener noreferrer">
+                    <a href="https://drive.google.com/file/d/1iUTnSo8pR5G-ExGhL6IjkxPUjovEg0UK/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                       <Button className="m-2" variant="outline-success">
                         My Resume
                       </Button>
